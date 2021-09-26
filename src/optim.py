1e55d7a9a6fcecdaae6a725fd2c42e497378a77d
Joaquin Rapela <joacorapela@gmail.com>
2021-09-26 13:40:26 +0100

    added constrained optimization

--- src/optim.py
+++ src/optim.py
@@ -24,26 +24,39 @@ def maximize_LBFGSB(func, x0, bounds, optim_params):
     niter = optim_res[2]["nit"]
     return {"maximum": maximum, "maximum_x": maximum_x, "nfeval": nfeval, "niter": niter}
 
-def maximize_LBFGS(x, eval_func, optim_params):
+# def maximize_LBFGS_torch(x, eval_func, bounds, n_iter=1000, params_change_tol=1e-6, max_LBFGS_iter=1):
+def maximize_LBFGS_torch(x, eval_func, bounds, n_iter=1,
+                         params_change_tol=1e-6, max_LBFGS_iter=1000):
     # note: when eval_func should use params
     for i in range(len(x)):
         x[i].requires_grad = True
-    optimizer = torch.optim.LBFGS(x, **optim_params)
+    optimizer = torch.optim.LBFGS(x, max_iter=max_LBFGS_iter)
+
     def closure():
         optimizer.zero_grad()
         curEval = -eval_func()
-        print(curEval)
-        # curEval.backward(retain_graph=True)
         curEval.backward()
-        return curEval
-    optimizer.step(closure)
+        return  curEval
+
+    params_change = torch.tensor(float("inf"))
+    i = 0
+    while i<n_iter and params_change>params_change_tol:
+        old_x = x[0].clone()
+        optimizer.step(closure)
+        print("params_change:", params_change.item())
+#         with torch.no_grad():
+#             for i, xi in enumerate(x[0]):
+#                 if bounds[i] is not None:
+#                     xi.clamp_(bounds[i][0], bounds[i][1])
+        params_change = torch.mean((old_x-x[0])**2)
+        i += 1
+
     for i in range(len(x)):
         x[i].requires_grad = False
     stateOneEpoch = optimizer.state[optimizer._params[0]]
     maximum = -eval_func()
-    maximum_x = x.clone()
+    maximum_x = x[0].clone()
     nfeval = stateOneEpoch["func_evals"]
     niter = stateOneEpoch["n_iter"]
     return {"maximum": maximum, "maximum_x": maximum_x, "nfeval": nfeval, "niter": niter}
-
 
